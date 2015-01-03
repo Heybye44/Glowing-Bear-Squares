64ecaf3e3bfddf07351d46643cacabf2e6789ac8
heybye44 <heybye44@gmail.com>
2015-01-03 02:33:56 +0000

    Buggy

--- www/gamemain.js
+++ www/gamemain.js
@@ -1,21 +1,50 @@
 console.log("Game starting");
 
 var g = new Phaser.Game(480, 800, Phaser.AUTO, '', {preload: preload, create: create, update: update});
+var score = 0;
+var badtile;
+var goodtile;
+var scoretxt;
+var endmsg;
+var dead;
+var plus;
 
 function preload() {
     g.load.image("tile1", "images/tile1.png");
     g.load.image("tile2", "images/tile2.png");
+    g.load.image("bkgr", "images/background.jpg")//This pic is CC-3.0
+    console.info("Preload Finished");
 }
 
 function create() {
-    for (var x = 0; x <= 6;x++){
-        
-        
-        g.add.sprite(x + 0.125, 0, "tile2");
-    };
+    g.add.sprite(0, 0, "bkgr");
+    
+    badtile = g.add.sprite(0 , 0, "tile2");
+    badtile.inputEnabled = true;
+    g.add.badtile;
+    
+    goodtile = g.add.sprite(0, 0, "tile1");
+    goodtile.inputEnabled = true;
+    
+    scoretxt = g.add.text(0, 770, "Score: 0", {fontSize: '32px', fill: '#FFFFFF'});
+    console.info("Create Finished");
 }
 
 function update() {
+    badtile.events.onInputDown.add(listener(), this);
+    goodtile.events.onInputDown.add(listener(), this);
     
+    scoretxt.text = "Score: " + score;
 }
-
+function listener() {
+    dead = function() {
+        badtile.kill();
+        goodtile.kill();
+        endmsg = g.add.text(g.world.centerX, g.world.centerY, "You lost. Your score is: " + score, {fontSize: '64px', fill: '#FF0000'});
+        
+    }
+    
+    plus = function() {
+        score++;
+    }
+}
